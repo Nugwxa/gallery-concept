4b40f1035e747e07184c2ca1285c0de1ff247e14
OJ-UOC <130554353+OJ-UOC@users.noreply.github.com>
2023-04-14 13:23:03 +0100

    Added Responsive feature

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let debugMessage = "I WORK!!!";
+let goodMessage = "I WORK!!!";
+let badMessage = "Nope :(";
 //console.log(debugMessage);
 
 const member1 = document.querySelector('#member-1-default');
@@ -8,8 +9,7 @@ const member4 = document.querySelector('#member-4-default');
 const member5 = document.querySelector('#member-5-default');
 const member6 = document.querySelector('#member-6-default');
 
-let screenSize;
-let targetScreenSize
+
 
 // img/face(straight).jpg -> DEFAULT
 // img/face(looking-left).jpg -> LOOKING LEFT
@@ -17,69 +17,156 @@ let targetScreenSize
 // img/face(looking-up).jpg -> LOOKING UP
 // img/face(looking-down).jpg -> LOOKING DOWN
 
-if (screenSize> targetScreenSize)
+const mediaQuery = window.matchMedia('(max-width: 768px)');
 
 
-member1.addEventListener('mouseover', function(){
-    member2.src = "img/face(looking-left).jpg";
-    member4.src = "img/face(looking-up).jpg";
-});
+//correctness
 
-member1.addEventListener('mouseout', function(){
-    member2.src = "img/face(straight).jpg";
-    member4.src = "img/face(straight).jpg";
-});
+let screenIsSmall = false;
 
-member2.addEventListener('mouseover', function(){
-    member1.src = "img/face(looking-right).jpg";
-    member3.src = "img/face(looking-left).jpg";
-    member5.src = "img/face(looking-up).jpg";
-});
+let handleMedia =  () => {
 
-member2.addEventListener('mouseout', function(){
-    member1.src = "img/face(straight).jpg";
-    member3.src = "img/face(straight).jpg";
-    member5.src = "img/face(straight).jpg";
-});
+    //When On Smaller Screens
+if (mediaQuery.matches){
+    screenIsSmall = true;
+    console.log(goodMessage);
 
-member3.addEventListener('mouseover', function(){    
-    member2.src = "img/face(looking-right).jpg";
-    member6.src = "img/face(looking-up).jpg";
-});
+} else{
+    screenIsSmall = false;
 
-member3.addEventListener('mouseout', function(){
-    member2.src = "img/face(straight).jpg";
-    member6.src = "img/face(straight).jpg";
-});
+    console.log(badMessage);
+    
+}
+};
 
-member4.addEventListener('mouseover', function(){   
-    member1.src = "img/face(looking-down).jpg";
-    member5.src = "img/face(looking-left).jpg";
-});
+handleMedia();
 
-member4.addEventListener('mouseout', function(){
-    member1.src = "img/face(straight).jpg";
-    member5.src = "img/face(straight).jpg";
-});
+//If window detects a change run
+window.addEventListener('change', handleMedia);
 
-member5.addEventListener('mouseover', function(){
-    member4.src = "img/face(looking-right).jpg";
-    member6.src = "img/face(looking-left).jpg";
-    member2.src = "img/face(looking-down).jpg";
-});
 
-member5.addEventListener('mouseout', function(){
-    member4.src = "img/face(straight).jpg";
-    member6.src = "img/face(straight).jpg";
-    member2.src = "img/face(straight).jpg";
-});
+    if (screenIsSmall){
 
-member6.addEventListener('mouseover', function(){
-    member3.src = "img/face(looking-down).jpg";
-    member5.src = "img/face(looking-right).jpg";
-});
+        member1.addEventListener('mouseover', function(){
+            member2.src = "img/face(looking-up).jpg";
+        });
+    
+        member1.addEventListener('mouseout', function(){
+            member2.src = "img/face(straight).jpg";
+        });
+    
+        member2.addEventListener('mouseover', function(){
+            member1.src = "img/face(looking-down).jpg";
+            member3.src = "img/face(looking-up).jpg";
+        });
+    
+        member2.addEventListener('mouseout', function(){
+            member1.src = "img/face(straight).jpg";
+            member3.src = "img/face(straight).jpg";
+        });
+        member3.addEventListener('mouseover', function(){
+            member2.src = "img/face(looking-down).jpg";
+            member4.src = "img/face(looking-up).jpg";
+        });
+    
+        member3.addEventListener('mouseout', function(){
+            member2.src = "img/face(straight).jpg";
+            member4.src = "img/face(straight).jpg";
+        });
+        member4.addEventListener('mouseover', function(){
+            member3.src = "img/face(looking-down).jpg";
+            member5.src = "img/face(looking-up).jpg";
+        });
 
-member6.addEventListener('mouseout', function(){
-    member3.src = "img/face(straight).jpg";
-    member5.src = "img/face(straight).jpg";
-});
+        member4.addEventListener('mouseout', function(){
+            member3.src = "img/face(straight).jpg";
+            member5.src = "img/face(straight).jpg";
+        });
+    
+        member5.addEventListener('mouseover', function(){
+            member4.src = "img/face(looking-down).jpg";
+            member6.src = "img/face(looking-up).jpg";
+        });
+
+        member5.addEventListener('mouseout', function(){
+            member4.src = "img/face(straight).jpg";
+            member6.src = "img/face(straight).jpg";
+        });
+
+        member6.addEventListener('mouseover', function(){
+            member5.src = "img/face(looking-down).jpg";
+        });
+    
+        member6.addEventListener('mouseout', function(){
+            member5.src = "img/face(straight).jpg";
+        });
+
+
+    } else{
+        
+
+    member1.addEventListener('mouseover', function(){
+        member2.src = "img/face(looking-left).jpg";
+        member4.src = "img/face(looking-up).jpg";
+    });
+    
+    member1.addEventListener('mouseout', function(){
+        member2.src = "img/face(straight).jpg";
+        member4.src = "img/face(straight).jpg";
+    });
+    
+    member2.addEventListener('mouseover', function(){
+        member1.src = "img/face(looking-right).jpg";
+        member3.src = "img/face(looking-left).jpg";
+        member5.src = "img/face(looking-up).jpg";
+    });
+    
+    member2.addEventListener('mouseout', function(){
+        member1.src = "img/face(straight).jpg";
+        member3.src = "img/face(straight).jpg";
+        member5.src = "img/face(straight).jpg";
+    });
+    
+    member3.addEventListener('mouseover', function(){    
+        member2.src = "img/face(looking-right).jpg";
+        member6.src = "img/face(looking-up).jpg";
+    });
+    
+    member3.addEventListener('mouseout', function(){
+        member2.src = "img/face(straight).jpg";
+        member6.src = "img/face(straight).jpg";
+    });
+    
+    member4.addEventListener('mouseover', function(){   
+        member1.src = "img/face(looking-down).jpg";
+        member5.src = "img/face(looking-left).jpg";
+    });
+    
+    member4.addEventListener('mouseout', function(){
+        member1.src = "img/face(straight).jpg";
+        member5.src = "img/face(straight).jpg";
+    });
+    
+    member5.addEventListener('mouseover', function(){
+        member4.src = "img/face(looking-right).jpg";
+        member6.src = "img/face(looking-left).jpg";
+        member2.src = "img/face(looking-down).jpg";
+    });
+    
+    member5.addEventListener('mouseout', function(){
+        member4.src = "img/face(straight).jpg";
+        member6.src = "img/face(straight).jpg";
+        member2.src = "img/face(straight).jpg";
+    });
+    
+    member6.addEventListener('mouseover', function(){
+        member3.src = "img/face(looking-down).jpg";
+        member5.src = "img/face(looking-right).jpg";
+    });
+    
+    member6.addEventListener('mouseout', function(){
+        member3.src = "img/face(straight).jpg";
+        member5.src = "img/face(straight).jpg";
+    });
+    }
+
